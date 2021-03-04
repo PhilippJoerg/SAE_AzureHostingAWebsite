ceb3f853074769acd4189aba0acbbeff85e4f4c7
Philipp Jörg <43471899+PhilippJoerg@users.noreply.github.com>
2021-03-04 11:22:22 +0100

    added WebsiteDotNet

---
+++ WebsiteDotNet/aspnetcoreapp/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function handleFormSubmit(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    const formJSON = Object.fromEntries(data.entries());
+  
+    const results = document.querySelector('.results pre');
+    results.innerText = JSON.stringify(formJSON, null, 2);
+  
+    console.log(JSON.stringify(formJSON));
+  }
+  
+  const form = document.querySelector('.contact-form');
+  form.addEventListener('submit', handleFormSubmit);
